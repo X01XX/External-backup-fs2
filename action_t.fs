aa11cea9a476ffdd1207c8174beca780cdc430be
Earl Dukerschein <earl.dukerschein@wisc.edu>
2026-06-20 18:02:28 -0500

    Add files via upload

--- action_t.fs
+++ action_t.fs
@@ -1,4 +1,23 @@
 
-: action-tests
+: action-test-basic
+    action-new      \ act
 
+    cr dup .action cr
+
+    s" s1010->s1010" sample-from-string-a   \ act smpl1
+    over action-add-sample                  \ act bool
+    if else ." Did not return true?" abort then
+
+    cr dup .action cr
+
+    action-deallocate
+
+    \ Check for memory leaks.
+    structinfo-list-store structinfo-list-project-deallocated
+
+    cr ." action-test-basic - Ok"
+;
+
+: action-tests
+    action-test-basic
 ;
